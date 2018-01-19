b8b6c43146e1d9ec13c4b5f4d33cdd6cd83ea874
Łukasz Harasimowicz <dev@harnash.eu>
2018-01-19 17:16:46 +0100

    Bump version: 0.1.0 → 0.1.1

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ else
 endif
 
 ifeq ($(VERSION),)
-	VERSION = "0.1.0"
+	VERSION = "0.1.1"
 endif
 
 docker_build:
--- server/common/version.py
+++ server/common/version.py
@@ -2,4 +2,4 @@ class VersionInfo(object):
     """Current version info"""
     repo_name = 'mistats'
     service_name = 'mistats'
-    version = '0.1.0'
+    version = '0.1.1'
