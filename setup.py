8423d8c14fc7b5a0ab365b2dbfda9eed97f47266
Łukasz Harasimowicz <dev@harnash.eu>
2018-01-19 18:25:37 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='purifier2prometheus',
-    version='0.1.1',
+    version='0.1.2',
     packages=['server'],
     url='https://github.com/harnash/mistats',
     license='MIT',
