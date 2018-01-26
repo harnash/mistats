0dae9552728442b404c4ec7ffff98f9bba239ed3
Łukasz Harasimowicz <dev@harnash.eu>
2018-01-26 22:03:03 +0100

    Updated device model

--- server/models/device.py
+++ server/models/device.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 from models import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
 
 
 class Device(Base):
     __tablename__ = 'devices'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     type = Column(String(length=255), nullable=False)
     address = Column(String(length=255), nullable=False)
+    enabled = Column(Boolean(), default=True)
+    last_seen = Column(TIMESTAMP(), nullable=False, default=datetime.utcnow)
+    updated = Column(TIMESTAMP(), nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    added = Column(TIMESTAMP(), nullable=False, default=datetime.utcnow)
