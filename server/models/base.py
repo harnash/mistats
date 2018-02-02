6986475c658f6711df72237251193b9b5a59ed03
Łukasz Harasimowicz <dev@harnash.eu>
2018-02-02 22:01:31 +0100

    Fixed JSON serialization

--- server/models/base.py
+++ server/models/base.py
@@ -1,3 +1,18 @@
+from collections import OrderedDict
+
+from hug.output_format import json_convert
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
+
+@json_convert(Base)
+def device_json(item: Base):
+    d = OrderedDict()
+    for column in item.__table__.columns:
+        d[column.name] = str(getattr(item, column.name))
+
+    return d
+
+
