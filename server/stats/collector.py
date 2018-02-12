97b20ed463a0baae7dd1e22e6a526981c6be48cf
Łukasz Harasimowicz <dev@harnash.eu>
2018-02-12 22:30:44 +0100

    Better error handling when gathering device stats

--- server/stats/collector.py
+++ server/stats/collector.py
@@ -2,7 +2,9 @@ from inspect import getmembers
 from prometheus_client.core import GaugeMetricFamily
 from enum import Enum
 from miio.discovery import create_device
+from miio.device import DeviceError, DeviceException
 
+from common.logging import Logger
 from db import SQLAlchemy
 from models import Device
 
@@ -13,6 +15,7 @@ class MiioDeviceCollector(object):
     def __init__(self, db: SQLAlchemy, prefix: str):
         self.__prefix = prefix
         self.__db = db
+        self.__log = Logger('mistats')
 
     @staticmethod
     def get_class(module, kls):
@@ -33,11 +36,30 @@ class MiioDeviceCollector(object):
             if info_class is None:
                 continue
             ip, _ = device.address.split(':', 1)
-            dev_info = create_device(ip, info_class)
+            try:
+                dev_info = create_device(ip, info_class)
+            except DeviceError as e:
+                self.__log.error("error connecting to a device", addr=ip, device=info_class,
+                                 exc_info=e, stack_info=True)
+                continue
+            except DeviceException as e:
+                self.__log.error("exception connecting to a device", addr=ip, device=info_class,
+                                 exc_info=e, stack_info=True)
+                continue
+
             status_method = getattr(dev_info, "status", None)
             label_values = [device.address, device.type, device.identifier]
             if callable(status_method):
-                status = status_method()
+                try:
+                    status = status_method()
+                except DeviceError as e:
+                    self.__log.error("error querying device status", addr=ip, device=info_class,
+                                     exc_info=e, stack_info=True)
+                    continue
+                except DeviceException as e:
+                    self.__log.error("exception querying device status", addr=ip, device=info_class,
+                                     exc_info=e, stack_info=True)
+                    continue
 
                 for name, value in getmembers(status.__class__):
                     if not isinstance(value, property):
